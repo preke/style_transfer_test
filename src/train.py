9df795899f43534570a2fd891ef5cd6e628fbdb8
preke <geek00021@gmail.com>
2019-04-04 22:37:54 +0800

    .

--- src/train.py
+++ src/train.py
@@ -127,7 +127,10 @@ def eval_vae(model, eval_iter, args, step, cur_epoch, iteration):
         feature    = Variable(sample)
         _input     = feature[:, :-1]
         target     = feature[:, 1:]
+        print(_input.size())
+        print(target.size())
         logp, mean, logv, z = model(_input, length)
+        print(logp.size())
         NLL_loss, KL_loss, KL_weight = loss_fn(logp, target,
             length, mean, logv, args.anneal_function, step, args.k, args.x0, model.pad_idx)
             
