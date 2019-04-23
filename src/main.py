8f4db477963d3776a4c9b273683b9b7f2d136e25
preke <geek00021@gmail.com>
2019-04-23 15:42:56 +0800

    .

--- src/main.py
+++ src/main.py
@@ -89,7 +89,7 @@ args.save_best     = True
 
 # Load data
 logger.info('Loading data begin...')
-text_field, label_field, train_data, train_iter, dev_data, dev_iter = load_data(mask_amazon_train, mask_amazon_test, args)
+text_field, label_field, train_data, train_iter, dev_data, dev_iter = load_data(mask_yelp_train, mask_yelp_test, args)
 text_field.build_vocab(train_data, dev_data, min_freq=5)
 label_field.build_vocab(train_data)
 logger.info('Length of vocab is: ' + str(len(text_field.vocab)))
