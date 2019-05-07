462eabd9aba142391418161af4f6b4931897bbc4
preke <geek00021@gmail.com>
2019-05-07 20:02:58 +0800

    .

--- src/train.py
+++ src/train.py
@@ -295,8 +295,8 @@ def train_cnn(train_iter, dev_iter, model, args):
     if args.cuda:
         model.cuda()
     parameters = list(filter(lambda p: p.requires_grad, model.parameters()))
-    optimizer = torch.optim.Adam(parameters, lr=args.lr)
-    # optimizer = torch.optim.SGD(parameters, lr=args.lr)
+    # optimizer = torch.optim.Adam(parameters, lr=args.lr)
+    optimizer = torch.optim.SGD(parameters, lr=args.lr)
     # optimizer = adabound.AdaBound(parameters, lr=args.lr, final_lr=0.1)
     steps = 0
     best_acc = 0
